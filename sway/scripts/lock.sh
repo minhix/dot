c2920ea24765b61b433b4f263d016f7fc3f0fbc5
Hieu Le <hieu.le@r3.group>
2022-09-28 01:03:00 +0200

    Add config for swaywm&waybar&wofi

---
+++ sway/scripts/lock.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+swaylock -f \
+  --image=/home/le/Downloads/Background/field_of_fire.jpg \
+  --inside-color=373445ff \
+  --ring-color=ffffffff \
+  --line-uses-inside \
+  --key-hl-color=d23c3dff \
+  --bs-hl-color=d23c3dff \
+  --separator-color=00000000 \
+  --inside-ver-color=fecf4dff \
+  --inside-wrong-color=d23c3dff \
+  --ring-ver-color=ffffffff \
+  --ring-wrong-color=ffffffff
